76028046316df05be74dff6d1e928eccba372708
cncardoso <cncardoso@yahoo.com>
2011-06-24 17:30:03 -0300

    added rails 3.1 compatibility and independent option for saas

--- lib/generators/nifty/layout/layout_generator.rb
+++ lib/generators/nifty/layout/layout_generator.rb
@@ -6,14 +6,18 @@ module Nifty
       argument :layout_name, :type => :string, :default => 'application', :banner => 'layout_name'
 
       class_option :haml, :desc => 'Generate HAML for view, and SASS for stylesheet.', :type => :boolean
+      class_option :saas, :desc => 'Generate SASS for stylesheet.', :type => :boolean
 
       def create_layout
         if options.haml?
           template 'layout.html.haml', "app/views/layouts/#{file_name}.html.haml"
-          copy_file 'stylesheet.sass', "public/stylesheets/sass/#{file_name}.sass"
         else
           template 'layout.html.erb', "app/views/layouts/#{file_name}.html.erb"
-          copy_file 'stylesheet.css', "public/stylesheets/#{file_name}.css"
+        end
+        if options.saas? || options.haml?
+          copy_file 'stylesheet.sass', "app/assets/stylesheets/#{stylesheet_name}.css.sass"
+        else
+          copy_file 'stylesheet.css', "app/assets/stylesheets/#{stylesheet_name}.css"
         end
         copy_file 'layout_helper.rb', 'app/helpers/layout_helper.rb'
         copy_file 'error_messages_helper.rb', 'app/helpers/error_messages_helper.rb'
@@ -23,6 +27,11 @@ module Nifty
 
       def file_name
         layout_name.underscore
+      end
+
+      # don't overwrite manifest file application.css
+      def stylesheet_name
+        file_name == 'application' ? 'application-wide' : file_name
       end
     end
   end
